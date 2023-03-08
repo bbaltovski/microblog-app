02f4cd3b24ae572e9f8ab43163916f4a04f40961
bbaltovski <121245996+bbaltovski@users.noreply.github.com>
2023-03-08 13:30:52 -0500

    Initial app

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ def create_app():
             )
             for entry in app.db.entries.find({})
         ]
-        return render_template("home.html", entries=entries_with_date)
+        return render_template("index.html", entries=entries_with_date)
 
     return app
